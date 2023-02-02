0a55a6b794acae2b5cc80bd92a7fc7d3439faac5
Popika <andreipop.acs@gmail.com>
2023-02-02 12:51:53 +0200

    Using setters for JPA Bidirectional Relationship - convenience methods

--- src/main/java/com/example/spring5recipeapp/model/Ingredient.java
+++ src/main/java/com/example/spring5recipeapp/model/Ingredient.java
@@ -16,7 +16,7 @@ public class Ingredient {
     @OneToOne(fetch = FetchType.EAGER)
     private UnitOfMeasure uom;
 
-    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom) {
         this.description = description;
         this.amount = amount;
         this.recipe = recipe;
