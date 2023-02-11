ef3a9fb73ddf485f5f62b707b7b1c524ad42d323
Popika <andreipop.acs@gmail.com>
2023-02-11 17:33:52 +0200

    Update CircleCI - config.yml

--- src/test/java/com/example/spring5recipeapp/repository/UnitOfMeasureRepositoryIT.java
+++ src/test/java/com/example/spring5recipeapp/repository/UnitOfMeasureRepositoryIT.java
@@ -3,21 +3,16 @@ package com.example.spring5recipeapp.repository;
 import com.example.spring5recipeapp.model.UnitOfMeasure;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 //@RunWith(SpringRunner.class) // brings up the Spring Context for Junit4 it's an alias for the SpringJUnit4ClassRunner
 //@ExtendWith(SpringExtension.class) // it's the Junit 5 equivalent
-// To be honest, I don't understand it's utility because it does work without any of it;
-@DataJpaTest //brings up an embedded DB and will configure Spring Data JPA for us
+@DataJpaTest //brings up an embedded DB and also configures the Spring Context
 class UnitOfMeasureRepositoryIT {
 
     @Autowired//because we bring up the Spring context;
